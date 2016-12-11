36388079cf6247ebb9d1e548988d9653d55cf425
Ogenbd <Ogenbd@gmail.com>
2016-12-11 14:25:28 +0200

    removed .. from img url

--- js/main.js
+++ js/main.js
@@ -23,18 +23,18 @@ var gBottomTxt=  {  align: 'center',
                     color: 'white',
                     shadow: '' }
 var gMemes = [
-            {id: 1, url: '../assets/img/1.jpg', keywords: ['sean', 'bean', 'lord of the rings']},
-            {id: 2, url: '../assets/img/2.jpg', keywords: ['animal', 'awkward', 'seal']},
-            {id: 3, url: '../assets/img/3.jpg', keywords: ['fry', 'cartoon', 'futurama']},
-            {id: 4, url: '../assets/img/4.jpg', keywords: ['crazy', 'cartoon']},
-            {id: 5, url: '../assets/img/5.jpg', keywords: ['buzz', 'cartoon', 'space']},
-            {id: 6, url: '../assets/img/6.jpg', keywords: ['winter is coming', 'person','game of thrones']},
-            {id: 7, url: '../assets/img/7.jpg', keywords: ['person', 'cartoon', 'first world problems']},
-            {id: 8, url: '../assets/img/8.jpg', keywords: ['person', 'bad luck']},
-            {id: 9, url: '../assets/img/9.jpg', keywords: ['kermit', 'cartoon']},
-            {id: 10, url: '../assets/img/10.jpg', keywords: ['baby', 'success']},
-            {id: 11, url: '../assets/img/11.jpg', keywords: ['kid', 'you tell me']},
-            {id: 12, url: '../assets/img/12.jpg', keywords: ['star trek', 'picard']}];
+            {id: 1, url: '/assets/img/1.jpg', keywords: ['sean', 'bean', 'lord of the rings']},
+            {id: 2, url: '/assets/img/2.jpg', keywords: ['animal', 'awkward', 'seal']},
+            {id: 3, url: '/assets/img/3.jpg', keywords: ['fry', 'cartoon', 'futurama']},
+            {id: 4, url: '/assets/img/4.jpg', keywords: ['crazy', 'cartoon']},
+            {id: 5, url: '/assets/img/5.jpg', keywords: ['buzz', 'cartoon', 'space']},
+            {id: 6, url: '/assets/img/6.jpg', keywords: ['winter is coming', 'person','game of thrones']},
+            {id: 7, url: '/assets/img/7.jpg', keywords: ['person', 'cartoon', 'first world problems']},
+            {id: 8, url: '/assets/img/8.jpg', keywords: ['person', 'bad luck']},
+            {id: 9, url: '/assets/img/9.jpg', keywords: ['kermit', 'cartoon']},
+            {id: 10, url: '/assets/img/10.jpg', keywords: ['baby', 'success']},
+            {id: 11, url: '/assets/img/11.jpg', keywords: ['kid', 'you tell me']},
+            {id: 12, url: '/assets/img/12.jpg', keywords: ['star trek', 'picard']}];
 
 
 function appInit() {
